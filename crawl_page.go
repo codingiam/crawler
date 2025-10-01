31edb20094b04287de12aec6f3f73ddc27b37847
Doru Budai <doru.budai@gmail.com>
2025-10-01 12:25:02 +0300

    Crawl

--- crawl_page.go
+++ crawl_page.go
@@ -2,14 +2,54 @@ package main
 
 import (
 	"fmt"
-	"log"
+	"net/url"
 )
 
 func crawlPage(rawBaseURL, rawCurrentURL string, pages map[string]int) {
-	html, err := getHTML(rawBaseURL)
+	baseURL, err := url.Parse(rawBaseURL)
 	if err != nil {
-		log.Fatal(err)
+		fmt.Printf("Error - crawlPage: couldn't parse base URL '%s': %v\n", rawCurrentURL, err)
+		return
 	}
 
-	fmt.Println(html)
+	currentURL, err := url.Parse(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("Error - crawlPage: couldn't parse current URL '%s': %v\n", rawCurrentURL, err)
+		return
+	}
+
+	if baseURL.Hostname() != currentURL.Hostname() {
+		return
+	}
+
+	normalizedURL, err := normalizeURL(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("Error - crawlPage: couldn't parse URL '%s': %v\n", rawBaseURL, err)
+		return
+	}
+
+	if _, ok := pages[normalizedURL]; ok {
+		pages[normalizedURL] += 1
+		return
+	}
+
+	pages[normalizedURL] = 1
+
+	fmt.Printf("crawling %s\n", rawCurrentURL)
+
+	html, err := getHTML(rawCurrentURL)
+	if err != nil {
+		fmt.Printf("Error - getHTML: %v", err)
+		return
+	}
+
+	urls, err := getURLsFromHTML(html, baseURL)
+	if err != nil {
+		fmt.Printf("Error - getURLsFromHTML: %v", err)
+		return
+	}
+
+	for _, url := range urls {
+		crawlPage(rawBaseURL, url, pages)
+	}
 }
