db4238c055bff04fc1573df10081431d71b72160
Doru Budai <doru.budai@gmail.com>
2025-10-01 10:47:52 +0300

    Get HTML

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	args := os.Args[1:]
+	argc := len(args)
+
+	if argc == 1 {
+		baseURL := args[0]
+		fmt.Println("starting crawl of:", baseURL)
+		os.Exit(0)
+	}
+
+	if argc < 1 {
+		fmt.Println("no website provided")
+	} else if argc > 1 {
+		fmt.Println("too many arguments provided")
+	}
+
+	os.Exit(1)
 }
